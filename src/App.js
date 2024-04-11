6ccc00f43465d00fe763dbcd01a15161c10105fe
Saritha Bora <saritha.bora@ncplcorp.com>
2024-04-11 19:55:47 +0530

    latest text added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Hai,Welcome To Devops</h1>
+          <h1>Hai,Welcome To Devops scrum call</h1>
           <h2>My React Deployment pipeline is ready</h2>
         <p>
           This is just a sample React app that we're going to deploy
